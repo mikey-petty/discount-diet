d05a0564d2fe15280dd578651e3099979840f247
mpetty9991 <47963967+mpetty9991@users.noreply.github.com>
2024-09-08 23:00:33 -0400

    Refreshing git cache

--- backend/diet_plan/serializers.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-from .models import Test
-
-
-class TestSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Test  # The model you want to serialize
-        fields = "__all__"  # All fields or specify ('id', 'name', etc.)
--- backend/diet_plan/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from .views import get_diet_plan, get_good_food, set_name
-
-urlpatterns = [
-    path('api/get_diet_plan/', get_diet_plan, name='get_diet_plan'),  # Define the correct path
-    path('api/get_good_food/', get_good_food, name='get_good_food'),  # Define the correct path
-    path('api/set_name/', set_name, name='set_name'),  # Define the correct path
-
-]
